2e989ddfad783ba18d488749b8b222189d86fd15
Elena <elenarjona@gmail.com>
2022-11-23 15:28:20 +0100

    array list columns accomplished

--- js/index.js
+++ js/index.js
@@ -1,77 +1,59 @@
-let aliveList = ["NOA", "CARMEN C.", "VEFLO", "ELENA", "ANA"];
-let deadList = [];
-console.log(aliveList);
+let alives = ["NOA", "CARMEN C.", "VEFLO", "ELENA", "ANA"];
+let deads = [];
+console.log(alives);
 //---AQUÍ LLAMAMOS A RELLENAR LISTAS---//
-window.addEventListener("kill", rellenarListas);
+window.addEventListener("load", rellenarListas);
 
 //---AQUÍ FUNCIÓN RELLENAR LISTAS---//
 function rellenarListas(){
     //Obtenemos los elementos HTML en los que mostramos los listados
-    let aliveList = document.getElementById("aliveList");
-    let deadList = document.getElementById("deadList");
+    let listaAlive = document.getElementById("listaAlive");
+    let listaDead = document.getElementById("listaDead");
 
     //Para los vivos
-    aliveList.innerHTML = "";
+    listaAlive.innerHTML = "";
     alives.forEach(
         (element) => {
-            aliveList.innerHTML += ('<li>' + element + '</li>');
+            listaAlive.innerHTML += ('<li>' + element + '</li>');
         }
     );
 
     //Para los muertos
-    deadList.innerHTML = "";
+    listaDead.innerHTML = "";
     deads.forEach(
         (element) => {
-            deadList.innerHTML += ('<li>' + element + '</li>');
+            listaDead.innerHTML += ('<li>' + element + '</li>');
         }
     );
 
 }
 
 //---AQUÍ FUNCIÓN MEDIR LONGITUD LISTA DE NOMBRES---//
-function calculeLengthArray(arrayList){
+function calculeLengthArray(alives){
     let lengthList = 0;
-    lengthList = arrayList.length;
+    lengthList = alives.length;
     return(lengthList);
 }
 
 //---AQUÍ COMPRUEBO SI MIDE BIEN LA LISTA---//
-let lenghtListName = calculeLengthArray(aliveList);
+let lenghtListName = calculeLengthArray(alives);
 console.log("Longitud de mi lista: ", lenghtListName);
 
 //---AQUÍ FUNCIÓN SACAR UN NOMBRE AL AZAR DE LA LISTA SIN REPETIR---//
-function killRandom(arrayList){
-    let lenghtList = arrayList.length;
-    let aleatorio = aliveList[Math.floor(Math.random()*aliveList.length)];
+function randomKill(){
+    let lenghtList = alives.length;
+    let aleatorio = alives[Math.floor(Math.random()*alives.length)];
 
     for(index=0;index<lenghtList;index++){
 
-    if(aliveList[index] === aleatorio) {
-        aliveList.splice(index, 1);
-        deads.unshift(deadList);
+    if(alives[index] === aleatorio) {
+        alives.splice(index, 1);
+        deads.unshift(aleatorio);
     }
+
+    rellenarListas();
     }
 
     //  AQUÍ SE METERÍA EL BOTÓN  //
     return(aleatorio);
 }
-
-// Llamar a la función rellenarListas para actualizar su contenido
-rellenarListas();
-
-//---AQUÍ COMPRUEBO SI FUNCIÓN KILLRANDOM FUNCIONA---//
-/* let killRandomPerson = killRandom(aliveList);
-console.log("Primera persona que ha muerto: ", killRandomPerson);
-let killRandomPerson1 = killRandom(aliveList);
-console.log("Segunda persona que ha muerto: ", killRandomPerson1);
-let killRandomPerson2 = killRandom(aliveList);
-console.log("Tercera persona que ha muerto: ", killRandomPerson2);
-let killRandomPerson3 = killRandom(aliveList);
-console.log("Cuarta persona que ha muerto: ", killRandomPerson3);
-let killRandomPerson4 = killRandom(aliveList);
-console.log("Quinta persona que ha muerto: ", killRandomPerson4);
-let killRandomPerson5 = killRandom(aliveList);
-console.log("Sexta persona que ha muerto:", killRandomPerson5); */
-//Creo que funciona porque no cuenta más allá de esas 5
-//---AQUÍ FUNCIÓN MANDAR A LA LISTA DE FIAMBRES---//
-
